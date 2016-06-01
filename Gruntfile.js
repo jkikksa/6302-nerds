abbd297159ac92f7451e36ffa9f8b98ccca0253f
jkikksa <jkikksa@gmail.com>
2016-06-01 23:14:51 +0300

    добавил задачи для grunt (imagemin, less, copy, watch)

---
+++ Gruntfile.js
@@ -0,0 +1,61 @@
+module.exports = function(grunt) {
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+
+
+  // grunt.registerTask('build', ['copy','less','imagemin','autoprefixer','csscomb', 'csso', 'concat', 'uglify']);
+
+  grunt.initConfig({
+    less: {
+      style: {
+        files: {
+          'dist/css/style.css': ['src/less/style.less']
+        }
+      }
+    },
+    watch: {
+      html_watch: {
+        files: ['src/*.html'],
+        tasks: ['copy'],
+      },
+      less_watch: {
+        files: '**/*.less',
+        tasks: ['less', 'autoprefixer', 'csscomb', 'csso']
+      }
+    },
+    copy: {
+      main: {
+        expand: true,
+        cwd: 'src/',
+        src: ['**/*.html'],
+        dest: 'dist/',
+      },
+    },
+    imagemin: {
+      dataimg: {
+        option: {
+          optimizationLevel: 3,
+        },
+        files: [{
+          expand: true,
+          cwd: 'src/img/data_image',
+          src: ['*.{png,jpg,gif,svg}'],
+          dest: 'src/img/data_image'
+        }]
+      },
+      img: {
+        option: {
+          optimizationLevel: 3,
+        },
+        files: [{
+          expand: true,
+          cwd: 'src/img',
+          src: ['*.{png,jpg,gif,svg}'],
+          dest: 'dist/img'
+        }]
+      }
+    },
+  })
+};
